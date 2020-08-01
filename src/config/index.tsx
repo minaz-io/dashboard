69dc5bc4e8a55e4f356b5fd8c1e12fc6ee8e08c7
Bertrand Juglas <b@juglas.name>
2020-08-01 20:20:27 +0200

    Update REST API config

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,5 +1,5 @@
 export const prefix =
-  'https://CB8FB876BD0841CF8622F8A8710CC790.blockchain.ocp.oraclecloud.com:443/restproxy1';
+  'https://CA83B146988245F2AAC7F92318AD109E.blockchain.ocp.oraclecloud.com:443/restproxy1';
 
 export const salt = 'auth';
 
